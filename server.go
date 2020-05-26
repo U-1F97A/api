b6201c71119afd33c24913f4be86bf7441c103e8
terfno <sueda.takahito@gmail.com>
2020-05-26 22:20:50 +0900

    :shirt:

--- server.go
+++ server.go
@@ -4,10 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello Glitch",
 		})
 	})
-	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run(":3000")
 }
