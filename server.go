9e24c45418e297ef877cf42cf9bd193a87a85dd1
terfno <sueda.takahito@gmail.com>
2020-05-26 22:12:34 +0900

    :tada: hello world

--- server.go
+++ server.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"html"
-	"net/http"
-)
-
-// https://go-example.glitch.me/
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Go!")
-}
-
-// https://go-example.glitch.me/love/Go
-func love(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", html.EscapeString(r.URL.Path[6:]))
-}
-
-// https://go-example.glitch.me/hacking
-func hacking(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hacking away on Go on Glitch.")
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	http.HandleFunc("/hacking", hacking)
-	http.HandleFunc("/love/", love)
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello Glitch",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
