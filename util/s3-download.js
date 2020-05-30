545e0cde2fbc604f6d67bc33814c9101e78852b8
terfno <sueda.takahito@gmail.com>
2020-05-31 07:23:44 +0900

    :zap: fix bug

--- util/s3-download.js
+++ util/s3-download.js
@@ -1,12 +1,10 @@
 const s3Client = require("./s3-client.js");
 
-function download(filename) {
+exports.download = (filename) => {
   const params = {
     Bucket: process.env.AWS_S3_BUCKET,
     Key: filename,
   };
 
   return s3Client.getObject(params).createReadStream();
-}
-
-module.exports = download;
+};
