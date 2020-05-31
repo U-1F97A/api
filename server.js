e871d2be8ef723c19f5b015942cbba51f653becf
terfno <sueda.takahito@gmail.com>
2020-05-31 15:44:02 +0900

    :+1: print log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const parser = require("body-parser");
+const express = require("express");
+var morgan = require("morgan");
 
 require("dotenv").config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
 app.use("/", handler);
 
 var port = process.env.PORT || 3000;
